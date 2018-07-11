4f4c9be14ea7c36adb4e739e74ef350e219f5959
vietvu-nguyen <vietvu.hcm@gmail.com>
2018-07-11 18:31:53 +0900

    change port

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)})
 app.put('/image',(req, res) => { image.handleImage(req, res, db)})
 app.post('/imageUrl', (req, res) =>{image.handleApiCall(req, res)})
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-	console.log(`app is running on ${port}`)
+// const port = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on ${process.env.PORT}`)
 })
 
